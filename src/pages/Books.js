82b33861a9210d1f74b149f5abe305976cb5a00f
Elufere Innocent <eluferedonatus@gmail.com>
2024-06-08 10:46:36 +0100

    commit

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -17,6 +17,15 @@ export function Books() {
         fetch();
     },[])
 
+    const handleDelete = async (id) => {
+        try{
+            await axios.delete("http://localhost:8800/books/"+id);
+            window.location.reload();
+        }catch(err){
+             
+        }
+    }
+
     return (
         <div>
             <h1>Karo Book Shop</h1>
@@ -27,8 +36,11 @@ export function Books() {
                         <h2>{book.title}</h2>
                         <p>{book.desc}</p>
                         <span>${book.price}</span>
-                        <button className='delete'>Delete</button>
-                        <button className='update'>Update</button>
+         
+                        <button className='delete' onClick={()=>handleDelete(book.id)}>Delete</button>
+                        <Link to={`/update/${book.id}`}>
+                            <button className='update'>Update</button>
+                        </Link>
                     </div>
                 ))}
             </div>
